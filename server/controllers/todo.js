ffa22f75c51bc7f354f7a693d6157b70df242e57
louisse2021 <100004777+louisse2021@users.noreply.github.com>
2023-05-12 14:02:28 -0400

    Final Project

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -9,8 +9,16 @@ const Todo = require("../models/todo")
  * Save document
  * <document>.save()
  *
+ *  * 
  */
 createTodo = async (req, res) => {
+  const documents = await Todo.create({task: 'value'})
+  console.log(documents)
+
+  if (documents.length > 0) {
+
+    return res.status(200).send({ data: documents, success: true })
+  }
   return res.status(500).json({ success: false })
 }
 
@@ -25,8 +33,8 @@ createTodo = async (req, res) => {
 getTodos = async (req, res) => {
   const documents = await Todo.find({})
   console.log(documents)
-  
-  if (!documents.length > 0) {
+
+  if (documents.length > 0) {
 
     return res.status(200).send({data:documents, success: true})
   }
@@ -43,6 +51,10 @@ getTodos = async (req, res) => {
  *
  */
 updateTodo = async (req, res) => {
+  const updatedTodo = await Todo.findOneAndDelete({ _id: req.params.id })
+  
+  if (!!updatedTodo)
+    return res.status(200).json({ success: true, data: updatedTodo })
   return res.status(500).json({ success: false })
 }
 
@@ -55,8 +67,14 @@ updateTodo = async (req, res) => {
  *
  */
 deleteTodo = async (req, res) => {
+  const foundTodo = await Todo.findOneAndDelete({_id: req.params.id})
+    
+  if (!!foundTodo)
+    return res.status(200).json({ success: true, data: foundTodo })
+ 
   return res.status(500).json({ success: false })
-}
+  }
+  
 
 module.exports = {
   createTodo,
